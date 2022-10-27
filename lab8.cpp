a0b1406a033798ba41a0b7ce388877566626bc22
Stepan <58824485+StepanSa@users.noreply.github.com>
2022-10-27 10:36:00 +0300

    Update lab8.cpp

--- lab8.cpp
+++ lab8.cpp
@@ -4,9 +4,29 @@
 #include <sys/resource.h>
 #include <signal.h>
 
-int main()
-{
+void *func(void * a) {
+    int ans;
+    std::cout << "Open pages in browser?(1/0): ";
+    std::cin >> ans;
+    if(ans == 1){
+        system("open https://www.google.com/search?q=wikipedia+english&client=firefox-b-lm&ei=50hZY6nFJNe0rgTV-4zgDA&oq=wikipedia+eng&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQARgAMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEOgoIABBHENYEELADOgcIABCwAxBDSgQIQRgASgQIRhgAUMMFWN4MYJgcaAFwAXgAgAGEAYgBnAOSAQMzLjGYAQCgAQHIAQrAAQE&sclient=gws-wiz-serp");
+        system("open https://duckduckgo.com/?t=ffab&q=wikipedia&ia=web");
+        system("open https://www.bing.com/search?form=MOZLBR&pc=MOZI&q=wikipedia");
+        system("open https://sg.search.yahoo.com/search;_ylt=Awrx_Z62SFljjswP_joi4gt.;_ylc=X1MDMjExNDcwODAwMgRfcgMyBGZyAwRmcjIDc2ItdG9wLXNlYXJjaARncHJpZANtN0owdGRZalJuLlFDWGxicklvdWtBBG5fcnNsdAMwBG5fc3VnZwMxMARvcmlnaW4Dc2cuc2VhcmNoLnlhaG9vLmNvbQRwb3MDMARwcXN0cgMEcHFzdHJsAzAEcXN0cmwDOQRxdWVyeQN3aWtpcGVkaWEEdF9zdG1wAzE2NjY3OTU2OTY-?p=wikipedia&fr=sfp&iscqry=&fr2=sb-top-search");  
+    }
+}
+
+int main() {
     pthread_t thread1;
+    pthread_create(&thread1, NULL, func, NULL);
+    kill(thread1, SIGSTOP);
+    sleep(5);
+    int t;
+    std::cout << "Suspend:";
+    std::cin >> t;
+    if (t == 1) kill(thread1, SIGSTOP);
+
+
     std::cout << "Do you want to change the priority(1/0)?: ";
     int ans;
     std::cin >> ans;
@@ -25,16 +45,7 @@ int main()
         }
     }
 
-    std::cout << "Open pages in browser?(1/0): ";
-    std::cin >> ans;
-    if(ans == 1){
-        system("open https://www.google.com/search?q=wikipedia+english&client=firefox-b-lm&ei=50hZY6nFJNe0rgTV-4zgDA&oq=wikipedia+eng&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQARgAMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEOgoIABBHENYEELADOgcIABCwAxBDSgQIQRgASgQIRhgAUMMFWN4MYJgcaAFwAXgAgAGEAYgBnAOSAQMzLjGYAQCgAQHIAQrAAQE&sclient=gws-wiz-serp");
-        system("open https://duckduckgo.com/?t=ffab&q=wikipedia&ia=web");
-        system("open https://www.bing.com/search?form=MOZLBR&pc=MOZI&q=wikipedia");
-        system("open https://sg.search.yahoo.com/search;_ylt=Awrx_Z62SFljjswP_joi4gt.;_ylc=X1MDMjExNDcwODAwMgRfcgMyBGZyAwRmcjIDc2ItdG9wLXNlYXJjaARncHJpZANtN0owdGRZalJuLlFDWGxicklvdWtBBG5fcnNsdAMwBG5fc3VnZwMxMARvcmlnaW4Dc2cuc2VhcmNoLnlhaG9vLmNvbQRwb3MDMARwcXN0cgMEcHFzdHJsAzAEcXN0cmwDOQRxdWVyeQN3aWtpcGVkaWEEdF9zdG1wAzE2NjY3OTU2OTY-?p=wikipedia&fr=sfp&iscqry=&fr2=sb-top-search");  
-    }
-    pthread_create(&thread1, NULL, NULL, NULL);
-    kill(thread1, SIGINT);
-    kill(thread1, SIGCONT);
+    // kill(thread1, SIGINT);
+    // kill(thread1, SIGCONT);
     pthread_join( thread1, NULL);
 }
